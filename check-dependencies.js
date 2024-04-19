f74625597ec89e74adc9202292d409e1940c3114
HaraldB-T <Harald.Bastiaanse@triggi.com>
2024-04-19 15:52:41 +0200

    various changes [squashed]

--- check-dependencies.js
+++ check-dependencies.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJson = JSON.parse(fs.readFileSync(path.join(process.env.MAIN_REPO_PATH, 'package.json')));
-const approvedPackages = JSON.parse(fs.readFileSync(path.join(process.env.TEST_REPO_PATH, 'approved-libraries.json')));
+let packageJson;
+try {
+	packageJson = JSON.parse(fs.readFileSync(path.join(process.env.MAIN_REPO_PATH, 'package.json')));
+} catch (err) {
+	console.error('package.json not found or is not valid JSON');
+	process.exit(1);
+}
+const approvedLibraries = JSON.parse(fs.readFileSync(path.join(process.env.WORKFLOW_REPO_PATH, 'approved-libraries.json')));
 
 const badLibraries = [];
-for (const [name, version] of Object.entries(package.dependencies || {})) {
+for (const [name, version] of Object.entries(packageJson.dependencies || {})) {
 	// Later we may want a more sophisticated test involving version as well
 	if (!approvedLibraries[name]) {
 		badLibraries.push(name);
